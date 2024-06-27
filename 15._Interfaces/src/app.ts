2ca7ab79358da63bd5aca95d12a748b4239da611
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2024-06-27 20:12:04 +0200

    Add interfaces

---
+++ 15._Interfaces/src/app.ts
@@ -0,0 +1,58 @@
+// Interface
+
+interface isPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: isPerson = {
+  name: "Mario",
+  age: 30,
+  speak(text: string) {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log(`I spent ${amount}`);
+    return amount;
+  },
+};
+
+import { Invoice } from "./models/invoice.js";
+
+const invoiceOne = new Invoice("Mario", "Work on website", 200);
+const invoiceTwo = new Invoice("Luigi", "Work on database", 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+
+invoices.forEach((invoice) => console.log(invoice.format()));
+
+// invoices.forEach((invoice) => console.log(invoice.details));
+
+class Receipt {
+  constructor(private client: string, private amount: number, private details: string) {}
+
+  toString() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const receiptOne = new Receipt("Mario", 200, "for work on website");
+
+console.log(receiptOne.toString());
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
